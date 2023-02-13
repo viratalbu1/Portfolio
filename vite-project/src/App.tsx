2a628936f111ac9ade9417f43df46813e80cfb0e
viratalbu1 <virat.singh199715@gmail.com>
2023-02-14 02:29:06 +0530

    Error in App.tsx

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import styles from './App.modules.scss?'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className={styles.root}>
+      
+    </div>
+  )
+}
+
+export default App
